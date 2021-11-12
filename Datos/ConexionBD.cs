23a6cea6a37b4833eb961b5251bf10adde507533
rycon123 <u201622564@upc.edu.pe>
2021-11-12 13:18:12 -0500

    borrocomentarios
    boroo comentario

--- Datos/ConexionBD.cs
+++ Datos/ConexionBD.cs
@@ -18,19 +18,5 @@ namespace Datos
         {
             return new SqlConnection(cadena);
         }
-
-        //public SqlConnection AbrirConexion()
-        //{
-        //    if (Conexion.State == ConnectionState.Closed)
-        //        Conexion.Open();
-        //    return Conexion;
-        //}
-
-        //public SqlConnection CerrarConexion()
-        //{
-        //    if (Conexion.State == ConnectionState.Open)
-        //        Conexion.Close();
-        //    return Conexion;
-        //}
     }
 }
